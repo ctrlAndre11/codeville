7b143b704e9a2dac48883473cf889fb1a10c2d20
Okonkwo Chikamso <chikamsodestiny2011@gmail.com>
2026-09-17 15:33:29 +0100

    feat My Portfolio

---
+++ portfolioo/script.js
@@ -0,0 +1,64 @@
+console.log("Hello, world!");
+
+// snake case
+let high_score = 100;
+let user_age = 30;
+
+// camel case
+let userName = "John Doe";
+let userAge = 30;
+
+let ageGrade = "17";
+
+console.log(ageGrade + 10)
+
+// snake case
+let light_on = true;
+let is_class_on = false;
+
+console.log(light_on);
+console.log(is_class_on);
+
+let my_wife = null;
+console.log(my_wife);
+
+let my_score_today
+console.log(my_score_today);
+
+let my_scores = [85, 90, 78, 92];
+console.log(my_scores);
+
+let my_friends = ["Alice", "Bob", "Charlie"];
+console.log(my_friends);
+
+let student = {
+    name: "Chikamso Okonkwo",
+    age: 15,
+    school: "Hillrange Secondary School",
+    hobbies: ["Reading", "Watching movies", "Gaming", "Travelling"],
+    is_married: false
+};
+
+console.log(student);
+
+
+console.log(typeof student);
+console.log(typeof student.name);
+console.log(typeof student.age);
+console.log(typeof student.school);
+console.log(typeof student.hobbies);
+console.log(typeof student.is_married);
+
+let pet = "cat";
+pet = "dog";
+console.log(pet);
+
+let treats = 3;
+treats = treats + 2;
+console.log(treats);
+
+let color = "blue"; 
+color = "green";
+console.log(color);
+
+
